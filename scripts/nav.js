99e3efc3e23bfbcd784eb525e914517559455829
مصطفى شملي <mstfa@mindPalace>
2018-11-05 14:57:37 -0500

    toggle menu icon on click

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,8 +1,15 @@
-function myFunction() {
+function responsive() {
 	var x = document.getElementById("topNav");
 	if (x.className === "nav") {
 		x.className += " responsive";
 	} else {
 		x.className = "nav";
+	}
+
+	var i = document.getElementById("icon");
+	if (i.className === "fa fa-bars") {
+		i.className = "fas fa-times";
+	} else {
+		i.className = "fa fa-bars";
 	}
 }
